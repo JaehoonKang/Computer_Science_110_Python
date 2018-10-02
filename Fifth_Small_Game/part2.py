f012d68e82076ed4c776f6ebee79f569746a2101
Jaehoon Kang <jhkang1015@gmail.com>
2018-10-01 23:30:01 -0400

    Change made to games + Comments added

--- Fifth_Small_Game/part2.py
+++ Fifth_Small_Game/part2.py
@@ -25,6 +25,8 @@ INPUT from keyboard:
   
 GIVEN:
 
+  DICE_MIN (int): 0 - the minimum value of the dice
+  DICE_MAX (int): 6 - the maximum value of the dice
 
 PROCESSING:
 
@@ -50,58 +52,89 @@ PROCESSING:
 FORMULAS:
 
 '''
-# IMPORT
+# IMPORT 
 import random
 
+# CONSTANTS
+# Define minimum & maximum value of the dice 
+DICE_MIN = 1
+DICE_MAX = 6
+
+# Design
 def main():
     
-  print('★★★★★★★Lucky Seven Game★★★★★★★')
+  print('This is a game based on lucky seven')
 
-  # Input
+  
+  
+  # Priming Read
+  # Input: Get an input from a user for a bet
   money_str = input('Please place your bet in positive whole dollars \
   OR press <Enter> to quit:')
-  
+
+  # Continuation Loop:
+  #   Keeping a program going until a user wants to quit(hit <Enter>)
   while money_str:
 
-    # Validation Loop: test the input if the input is greater than 0
+    # Validation Loop: to force the user to give a valid input
+    #   Whether the input is an integer and is greater than 0
     while not (money_str.isdigit() and int(money_str) > 0):
         
       print('INVALID INPUT:  Input whole numbers greater than 0 ONLY')
       money_str = input('Please place your bet in positive whole dollars \
       OR press <Enter> to quit:')
 
+    # Once the Validation is done, convert the value into an integer
     money = int(money_str)
 
+    # Create a table for Roll, Value, and Dollars
     print("%4s %8s %10s" %('Roll','Value','Dollars'))
 
-    # roll and pot setting
-    roll = 0
-    pot = {}
     
+    # roll and pot setting
+    # roll starts at 0 
+    roll = 0
+    # Using a dictionary for a pot
+    pot = {}
+
+    # While loop until money equals to 0
     while money !=0:
       
-      # dice and sum of dices setting
-      dice_1 = random.randrange (1,7)
-      dice_2 = random.randrange (1,7)
+      # dice and sum of dices setting (value)
+      dice_1 = random.randrange (DICE_MIN,DICE_MAX)
+      dice_2 = random.randrange (DICE_MIN,DICE_MAX)
       value = dice_1 + dice_2
+
+      # if value equals to 7
       if value == 7:
         money += 4
         
       else:
         money -= 1
-        
+
+      # roll increment by 1 each turn  
       roll += 1
+
+      # putting a key and corresponding value in a dictionary
       pot[roll] = money
-      
+
+      # print out roll, value and money
       print("%4d  %6d  %10.2f" %(roll, value, money))
     
     print("You became broke after %d rolls" %roll)
 
+    # using max() to find the maximum value of key and value in dictionary
     roll = max(pot, key=pot.get)
-    max_money = pot[roll]
+
+    # save the max value to max_Money
+    max_Money = pot[roll]
+    
+    # Display the sentence with the calculated outcome with % specifiers
     print("You should have quit after %d rolls when you have $%.2f" \
-            %(roll, max_money) )
-      
+            %(roll, max_Money) )
+    
+    # Continuation Read:
+    #   Keeps the program going as long as user enters input
     money_str = input('Please place your bet \
 in positive whole dollars OR press <Enter> to quit:')
     
--- Fifth_Small_Game/part3.py
+++ Fifth_Small_Game/part3.py
@@ -23,8 +23,8 @@ INPUT from keyboard:
   
 GIVEN:
 
-  FIRST = 1
-  DOLLAR = 100
+  FIRST = 1 - First Day
+  DOLLAR = 100 - to convert to a dollar
 
 PROCESSING:
 
@@ -54,35 +54,58 @@ DOLLAR = 100
 # Design
 def main():
 
+    # Present the purpose of the program
     print("Days must be a positive value greater than zero")
+
+    # Priming Read;
+    #  Get an input from a user for how many days they want to compute
     days_str = input("Enter the number of days \
 for which salary is to be computed:  ")
 
+    # Continuation Loop:
+    #   Keeping a program going until a user wants to quit(hit <Enter>) 
     while days_str:
         
-      # Validation Loop
+      # Validation Loop: to force the user to give a valid input
+      #   To determin whether the input is an integer or greater than 0
       while not (days_str.isdigit() and int(days_str) > 0):
         print("Days must be a positive value greater than zero")  
         days = input("Enter the valid number of days:  ")
-      # Convert to an integer  
+        
+      # Once the Validation is done, convert the value into an integer 
       days = int(days_str)  
       
-      
+      # Create a table for Day and Pay
       print("%6s %63s" %('Day', 'Pay'))
-      print("-------------------------------------------------------------\
-----------")
+      print(70 *"-")
+
+      # Display the first day
       print("%6d %62d" %(FIRST, FIRST)) 
-      
+
+      # Define a value as total for the total amount of money
       total = 0
+
+      # Using a for-loop to find each day and corresponding dollar
+      #   until the value meets the day the user inputs
       for inc in range(FIRST, days):
+          
+        # Each day, the amount doubles
         amount = 2**inc  
+        # Add the amount to the total money  
         total += amount
+        # Display each day and a corresponding the money
         print("%6d   %60d" %(inc +1, amount))
         
       print("")
-      dollar = (total + 1) / DOLLAR
-      print("In %d days a penny grows to $%9.2f" %(days, dollar))  
 
+      # Convert it to a dollar by dividing it by DOLLAR(100)
+      dollar = (total + 1) / DOLLAR
+
+      # Display the total amount and the last day
+      print("In %d days a penny grows to $%9.2f" %(days, dollar))
+      
+      # Continuation Read:
+      #   Keeps the program going as long as user enters input
       days_str = input("Enter the number of days \
 for which salary is to be computed:  ")
 
