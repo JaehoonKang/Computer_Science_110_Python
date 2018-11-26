f56ea7a06daac67d60eb9ccc36f81bb9667231c5
Jaehoon Kang <jhkang1015@gmail.com>
2018-11-25 20:55:54 -0500

    Guessing Game Part Donw

--- FinalProject/Guess.py
+++ FinalProject/Guess.py
@@ -13,19 +13,19 @@ class GuessingGame:
         
         def get_value():
             number = self.__entry_guess.get()
-            print(number)
-            
+    
+            if int(number) < self.__answer:
+                self.__label_right.config(text="Your guess is lower than the answer", fg="white", bg="blue")
+                ##print("Your guess is lower than the answer")
 
-        def compare_value():
-            number = self.__entry_guess.get()
-            if number < self.__answer:
-                print("Your guess is lower than the answer")
-
-            elif number > self.__answer:
-                print("Your guess is greater than the answer")
+            elif int(number) > self.__answer:
+                self.__label_right.config(text="Your guess is greater than the answer", fg="white", bg="black")
+                ##print("Your guess is greater than the answer")
 
             else:
-                print("Your answer is right!!")
+                self.__label_right.config(text="Your guess is exactly right! ★★★★★★", fg= "yellow", bg="red")
+                ##print("Your answer is right!!")
+                self.__window.quit()
         
         
         self.__upper_frame = Frame(self.__window, width=500, height = 400)
@@ -37,23 +37,26 @@ class GuessingGame:
         self.__label_intro = Label(self.__upper_frame, text="Welcome to the Guessing Game")
         self.__label_intro.grid(row=0, column =0)
 
+        self.__label_intro = Label(self.__upper_frame, text="Guess the number between 1 and 99")
+        self.__label_intro.grid(row=1, column =0)
+
 
         
 
-        self.__label_right = Label(self.__upper_frame, text="the answer you put is lower", fg="red")
-        self.__label_right.grid(row=1, column =0)
+        self.__label_right = Label(self.__upper_frame, text="####", fg="red", bg="yellow")
+        self.__label_right.grid(row=2, column =0)
 
         
 
         #var = IntVar()
 
         self.__entry_guess = Entry(self.__lower_frame)
+        #self.__entry_guess.config(command=compare_value)
         self.__entry_guess.grid(row=1, column=0)
 
         
         self.__button_input = Button(self.__lower_frame, text="enter", fg="white", bg="black", command=get_value)
         self.__button_input.grid(row=1, column=1)
-
 
         
 
