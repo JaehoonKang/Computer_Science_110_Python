d6dba088d2568b5328b89b49238ab37cb114512f
Jaehoon Kang <jhkang1015@gmail.com>
2018-11-25 20:42:29 -0500

    Guessing Game working on

--- FinalProject/Guess.py
+++ FinalProject/Guess.py
@@ -7,24 +7,55 @@ class GuessingGame:
 
     def __init__(self):
         self.__window = Tk()
+
+        self.__answer = random.randint(1,99)
+        print(self.__answer)
         
-        self.__upper_frame = Frame(self.__window, width=300, height = 250)
+        def get_value():
+            number = self.__entry_guess.get()
+            print(number)
+            
+
+        def compare_value():
+            number = self.__entry_guess.get()
+            if number < self.__answer:
+                print("Your guess is lower than the answer")
+
+            elif number > self.__answer:
+                print("Your guess is greater than the answer")
+
+            else:
+                print("Your answer is right!!")
+        
+        
+        self.__upper_frame = Frame(self.__window, width=500, height = 400)
         self.__upper_frame.grid(row=0, column=0)
 
-        self.__lower_frame = Frame(self.__window, width=300, height = 250)
+        self.__lower_frame = Frame(self.__window, width=500, height = 400)
         self.__lower_frame.grid(row=3, column=0)
 
         self.__label_intro = Label(self.__upper_frame, text="Welcome to the Guessing Game")
         self.__label_intro.grid(row=0, column =0)
 
-        self.__label_right = Label(self.__upper_frame, text="the answer you put is lower")
+
+        
+
+        self.__label_right = Label(self.__upper_frame, text="the answer you put is lower", fg="red")
         self.__label_right.grid(row=1, column =0)
+
+        
+
+        #var = IntVar()
 
         self.__entry_guess = Entry(self.__lower_frame)
         self.__entry_guess.grid(row=1, column=0)
 
-        self.__button_input = Button(self.__lower_frame, text="enter", fg="white", bg="black")
+        
+        self.__button_input = Button(self.__lower_frame, text="enter", fg="white", bg="black", command=get_value)
         self.__button_input.grid(row=1, column=1)
+
+
+        
 
         self.__window.mainloop()
 
